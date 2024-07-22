ffffc1d690ac61e530bc9e8223bd98fbd089bdb5
Yannick Tison <ytison@lfdj.com>
2024-07-22 11:26:41 +0000

    remove jacpkot withdrawal

--- anchor/programs/nft_lotery_no_code/src/instructions/mod.rs
+++ anchor/programs/nft_lotery_no_code/src/instructions/mod.rs
@@ -3,11 +3,9 @@ pub mod create_nft_v1;
 pub(crate) mod get_random_ticket_number;
 pub mod initialize_jackpot_vault;
 pub mod transfer_v1;
-pub mod withdraw_jackpot;
 
 pub use create_collection_v1::*;
 pub use create_nft_v1::*;
 pub(crate) use get_random_ticket_number::*;
 pub use initialize_jackpot_vault::*;
 pub use transfer_v1::*;
-pub use withdraw_jackpot::*;
